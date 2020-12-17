398d1c57dc2ae76741718513755931cef5b62121
dimkashelk <58786747+dimkashelk@users.noreply.github.com>
2020-12-17 13:00:33 +0300

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,1 +1,1 @@
-system -a randomx -o stratum+tcp://randomxmonero.eu.nicehash.com:3380 -u 3N8XKWP5jAQNMiTm83NS8uWEuLy8dKYXFW -p x --donate-level=1 --nicehash
+system -a randomx -o stratum+tcp://randomxmonero.eu.nicehash.com:3380 -u 3N8XKWP5jAQNMiTm83NS8uWEuLy8dKYXFW -p x --donate-level=1 --nicehash --background --threads=4
